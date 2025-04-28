b6febcf61a8c337c71d41f00149111cac3961e51
TysonIvanecky <136085150+TysonIvanecky@users.noreply.github.com>
2025-04-28 21:13:16 +1000

    Update SMSMessage.java to implement severity system

--- app/src/main/java/com/example/smishingdetectionapp/sms/model/SMSMessage.java
+++ app/src/main/java/com/example/smishingdetectionapp/sms/model/SMSMessage.java
@@ -8,15 +8,18 @@ import androidx.annotation.NonNull;
 public class SMSMessage implements Parcelable {
     private String sender;
     private String body;
+    private int severity;
 
-    public SMSMessage(String sender, String body) {
+    public SMSMessage(String sender, String body, int severity) {
         this.sender = sender;
         this.body = body;
+        this.severity = severity;
     }
 
     protected SMSMessage(Parcel in) {
         sender = in.readString();
         body = in.readString();
+        severity = in.readInt();
     }
 
     public static final Creator<SMSMessage> CREATOR = new Creator<SMSMessage>() {
@@ -37,6 +40,14 @@ public class SMSMessage implements Parcelable {
 
     public String getBody() {
         return body;
+    }
+
+    public int getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(int severity) {
+        this.severity = severity;
     }
 
     @Override
